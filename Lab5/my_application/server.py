15d130e0f6ee4b8a3f02d0b327346f040bce3f1c
Orla Fahy <c13520633@mydit.ie>
2015-10-14 16:23:14 +0100

    Lab Changes

--- Lab5/my_application/server.py
+++ Lab5/my_application/server.py
@@ -1,9 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
+def index():
+    return "Index File"
+
+@app.route("/Hello")
 def hello():
     return "Hello World!"
+    
+@app.route("/user/paul")
+def user():
+	return "User paul"
+
+@app.route("/post/80")
+def post():
+	return "Post 80"
 
 if __name__ == "__main__":
 #    app.run(host="0.0.0.0")
